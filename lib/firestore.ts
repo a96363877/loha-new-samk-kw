33b1860a1aff085f342baa68471e988491c59de7
a96363877 <a96363877@gmail.com>
2025-06-27 05:08:10 +0300

    Update firestore.ts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -3,13 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA6o3zVLpGEzKg_PI_NdB5DaAwG2WEjZkE',
-  authDomain: 'new-en-asmk.firebaseapp.com',
-  projectId: 'new-en-asmk',
-  storageBucket: 'new-en-asmk.firebasestorage.app',
-  messagingSenderId: '1072067984638',
-  appId: '1:1072067984638:web:7442b3033b07c55ef479af',
-  measurementId: 'G-202FP1QLNL',
+    apiKey: "AIzaSyB1Tpv9S00TO__RCkAN95ydnMQDR7yEb0A",
+  authDomain: "csa3-e2b6a.firebaseapp.com",
+  databaseURL: "https://csa3-e2b6a-default-rtdb.firebaseio.com",
+  projectId: "csa3-e2b6a",
+  storageBucket: "csa3-e2b6a.firebasestorage.app",
+  messagingSenderId: "328650323342",
+  appId: "1:328650323342:web:468ea6435238c0452be0df",
+  measurementId: "G-D32GDGT38Q"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
